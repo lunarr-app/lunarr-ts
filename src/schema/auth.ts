68f70bb7f6ecd7a3b48d7f988dda1adbc65b8b5d
Sayem Chowdhury <sayem314@gmail.com>
2023-04-05 17:51:44 +0600

    add schema

---
+++ src/schema/auth.ts
@@ -0,0 +1,27 @@
+import {Type, Static} from '@sinclair/typebox';
+
+export const UserLoginSchema = Type.Object({
+  username: Type.String({
+    minLength: 2,
+    maxLength: 16,
+  }),
+  password: Type.String({minLength: 6, maxLength: 32}),
+});
+
+export type UserLoginType = Static<typeof UserLoginSchema>;
+
+export const UserSignupSchema = Type.Object({
+  displayname: Type.String({minLength: 1, maxLength: 48}),
+  username: Type.String({
+    minLength: 2,
+    maxLength: 16,
+    pattern: '^[0-9a-zA-Z_-]+$',
+    description:
+      'Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.',
+  }),
+  email: Type.String({format: 'email'}),
+  password: Type.String({minLength: 6, maxLength: 32}),
+  sex: Type.Union([Type.Literal('male'), Type.Literal('female'), Type.Literal('unknown')], {default: 'unknown'}),
+});
+
+export type UserSignupType = Static<typeof UserSignupSchema>;

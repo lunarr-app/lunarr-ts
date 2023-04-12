69b8d8595a7da31764fe08ebdbdfcac6226e98b9
Sayem Chowdhury <sayem314@gmail.com>
2023-04-12 23:18:32 +0600

    refactor auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,52 +6,57 @@ import {RESTRICTED_USERNAMES} from '../lib/username.js';
 import {UserLoginSchema, UserLoginType, UserSignupSchema, UserSignupType} from '../schema/auth.js';
 import type {FastifyInstance} from 'fastify';
 
-/**
- * Create user index for faster query performance
- * Strength 2 means case insensitive search
- */
+// Create user index for faster query performance
+// Strength 2 means case insensitive search
 const collation = {locale: 'en', strength: 2};
 
 const auth = async (fastify: FastifyInstance) => {
   // User login
-  fastify.post<{Body: UserLoginType}>('/login', {schema: {body: UserLoginSchema}}, async (res, reply) => {
-    const user = await usersAccounts.findOne({username: res.body.username}, {collation});
-    if (user) {
-      const isValidPass = await argon2.verify(user.password, res.body.password);
-      if (isValidPass) {
-        return {api_key: user.api_key};
-      }
+  fastify.post<{Body: UserLoginType}>('/login', {schema: {body: UserLoginSchema}}, async (req, reply) => {
+    const user = await usersAccounts.findOne({username: req.body.username}, {collation});
+
+    // Check if the user exists and password is valid
+    if (user && (await argon2.verify(user.password, req.body.password))) {
+      return {api_key: user.api_key};
     }
 
-    reply.code(401).send('Invalid credentials');
+    // Return 401 Unauthorized error if invalid credentials
+    reply.status(401).send('Invalid username or password');
   });
 
   // User signup
-  fastify.post<{Body: UserSignupType}>('/signup', {schema: {body: UserSignupSchema}}, async (res, reply) => {
-    const user = await usersAccounts.findOne({username: res.body.username}, {collation});
+  fastify.post<{Body: UserSignupType}>('/signup', {schema: {body: UserSignupSchema}}, async (req, reply) => {
+    const user = await usersAccounts.findOne({username: req.body.username}, {collation});
+
+    // Check if the user already exists
     if (user) {
-      reply.code(409).send('An user already exists with that username.');
+      reply.status(409).send('An user already exists with that username.');
       return;
     }
 
-    if (RESTRICTED_USERNAMES.includes(res.body.username.toLowerCase())) {
-      reply.code(409).send('Username forbidden. Try different usernames.');
+    // Check if the username is restricted
+    if (RESTRICTED_USERNAMES.includes(req.body.username.toLowerCase())) {
+      reply.status(409).send('Username is not allowed. Please choose a different username.');
       return;
     }
 
-    const totalUsers = await usersAccounts.countDocuments();
+    // Hash the user password and insert into the database
+    const passwordHash = await argon2.hash(req.body.password);
+    const totalUsers = await usersAccounts.countDocuments(); // Get the total number of users in the database
     const timeNowIso = dayjs().toISOString();
 
-    res.body.password = await argon2.hash(res.body.password);
-    const created = await usersAccounts.insertOne({
-      ...res.body,
-      role: totalUsers === 0 ? 'admin' : 'subscriber',
+    await usersAccounts.insertOne({
+      ...req.body,
+      role: totalUsers === 0 ? 'admin' : 'subscriber', // If this is the first user, set the role to admin, otherwise set it to subscriber
       api_key: uuidv4(),
       created_at: timeNowIso,
       last_seen_at: timeNowIso,
       current_status: 'active',
+      password: passwordHash, // Store the password hash in the database
     });
-    reply.code(201).send(created);
+
+    // Return a success message
+    reply.send('User created successfully.');
   });
 };
 

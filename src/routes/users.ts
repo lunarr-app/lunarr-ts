300cf75b91af9aa4a9fa277fbe4ed50b6aa680f9
Sayem Chowdhury <sayem314@gmail.com>
2023-04-12 23:23:58 +0600

    improve me endpoint

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,13 +2,23 @@ import {usersAccounts} from '../lib/database.js';
 import type {FastifyInstance, RouteShorthandOptions} from 'fastify';
 
 const users = async (fastify: FastifyInstance, options: RouteShorthandOptions) => {
-  fastify.get('/me', options, async (res) => {
+  // Get user by api key
+  fastify.get('/me', options, async (res, reply) => {
+    // Get the user from the database by api key
     const user = await usersAccounts.findOne(
       {api_key: res.headers['x-api-key']},
       {
-        projection: {password: 0}, // emit password hash
+        projection: {password: 0}, // Exclude password hash from response
       },
     );
+
+    if (!user) {
+      // Return 404 error if user not found
+      reply.status(404).send('User not found');
+      return;
+    }
+
+    // Return user object if found
     return user;
   });
 };

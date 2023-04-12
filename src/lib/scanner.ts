335489192fc43bdff6f985f9d766c672a6a84dee
Sayem Chowdhury <sayem314@gmail.com>
2023-04-12 21:35:53 +0600

    add comments

--- src/lib/scanner.ts
+++ src/lib/scanner.ts
@@ -3,8 +3,17 @@ import {filenameParse} from '@ctrl/video-filename-parser';
 
 const VIDEO_EXTENSIONS = ['mp4', 'mov', 'avi', 'mkv', 'webm'];
 
+/**
+ * Scans a directory for video files and logs information about each file.
+ * @param {string} path - The path of the directory to scan.
+ * @returns {Promise<void>} - A promise that resolves when the scanning is complete.
+ */
 export const scanMediaDirectory = async (path: string): Promise<void> => {
+  // Use `opendir` instead of `readdir` to efficiently iterate over the contents of the directory.
+  // `opendir` returns an asynchronous iterator that allows us to iterate over the directory's contents one by one,
+  // without loading everything into memory at once. This makes it more memory-efficient and less error-prone than `readdir`.
   const directory = await opendir(path);
+
   for await (const entry of directory) {
     const entryPath = `${path}/${entry.name}`;
     if (entry.isDirectory()) {
@@ -12,8 +21,7 @@ export const scanMediaDirectory = async (path: string): Promise<void> => {
     } else {
       const extension = entry.name.split('.').pop()?.toLowerCase();
       if (extension && VIDEO_EXTENSIONS.includes(extension)) {
-        const videoName = entry.name.replace(/\.[^/.]+$/, '');
-        const videoInfo = filenameParse(videoName);
+        const videoInfo = filenameParse(entry.name);
         console.log(JSON.stringify(videoInfo, null, 2));
         // To-do: Search movies
       }

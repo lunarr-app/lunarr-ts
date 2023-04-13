ebdd010c2fe2b463cc739789362e07342eeb29bc
Sayem Chowdhury <sayem314@gmail.com>
2023-04-14 04:24:47 +0600

    update test

--- tests/app.ts
+++ tests/app.ts
@@ -1,12 +1,20 @@
 import test from 'ava';
 import app from '../src/app.js';
 
-test('GET /', async (t) => {
+test('GET / responds with 200 status and "hello world" message', async (t) => {
   const response = await app.inject({
     method: 'GET',
     url: '/',
   });
-
   t.is(response.statusCode, 200);
-  t.deepEqual(response.json(), {hello: 'world'});
+  t.deepEqual(JSON.parse(response.body), {hello: 'world'});
+});
+
+test('GET /users without x-api-key header returns 401 Unauthorized error', async (t) => {
+  const response = await app.inject({
+    method: 'GET',
+    url: '/users',
+  });
+  t.is(response.statusCode, 401);
+  t.deepEqual(JSON.parse(response.body), {error: 'Unauthorized', message: 'Invalid API key'});
 });

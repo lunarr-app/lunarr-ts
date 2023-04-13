160384ae218260f045f71c840e31283bb31d10f4
Sayem Chowdhury <sayem314@gmail.com>
2023-04-14 02:51:07 +0600

    refactor db

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -3,17 +3,20 @@ import {env} from './config.js';
 import type {UserTypeMongo} from '../schema/auth';
 import type {MovieList} from '../types/database';
 
-// Create the database
+// Create a new MongoClient instance with the MongoDB URI and options
 const mongo = new MongoClient(env.MONGODB_URI, {retryWrites: true, w: 'majority'});
 
-// Connect the database
+// Connect to the MongoDB database
 await mongo.connect();
 
-// Export collections
+// Ping the database to verify the connection
+await mongo.db().command({ping: 1});
+
+// Export MongoDB collections as typed objects
 export const usersAccounts = mongo.db().collection<UserTypeMongo>('users.accounts');
 export const moviesLists = mongo.db().collection<MovieList>('movies.lists');
 
-// Create indexes
+// Create indexes on relevant fields for improved query performance
 await usersAccounts.createIndexes([
   {
     key: {username: 1},

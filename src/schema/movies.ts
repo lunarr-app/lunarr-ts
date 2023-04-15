c176a71aec597234a46d1c3320e397333525089e
Sayem Chowdhury <sayem314@gmail.com>
2023-04-15 22:26:15 +0600

    refactor movies

--- src/schema/movies.ts
+++ src/schema/movies.ts
@@ -1,12 +1,29 @@
-import {Type} from '@sinclair/typebox';
+import {Type, Static} from '@sinclair/typebox';
+import {API_HEADERS, SCHEMA_SECUIRTY} from './auth.js';
 
-export const MovieResultsQuerySchema = Type.Object({
-  limit: Type.Number({
-    minimum: 1,
-    default: 20,
+export const MovieResultsQuery = {
+  headers: API_HEADERS,
+  querystring: Type.Object({
+    limit: Type.Number({
+      minimum: 1,
+      default: 20,
+    }),
+    page: Type.Number({
+      minimum: 1,
+      default: 1,
+    }),
   }),
-  page: Type.Number({
-    minimum: 1,
-    default: 1,
-  }),
-});
+  response: {
+    200: Type.Object({
+      results: Type.Array(Type.Any()),
+      page: Type.Number(),
+      total: Type.Number(),
+    }),
+  },
+  ...SCHEMA_SECUIRTY,
+};
+
+export type MovieResultsQueryType = {
+  Querystring: Static<typeof MovieResultsQuery.querystring>;
+  Reply: Static<(typeof MovieResultsQuery.response)[200]>;
+};

a180ee6e2b3217dfd7617fa42aaca4fd4f60cb6a
Sayem Chowdhury <sayem314@gmail.com>
2023-04-15 18:57:02 +0600

    refactor auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,17 +1,9 @@
 import {v4 as uuidv4} from 'uuid';
 import argon2 from 'argon2';
 import dayjs from 'dayjs';
-import {Static} from '@sinclair/typebox';
 import {usersAccounts} from '../lib/database.js';
 import {RESTRICTED_USERNAMES} from '../lib/username.js';
-import {
-  UserLoginSchema,
-  UserLoginSchema200,
-  UserLoginSchema401,
-  UserSignupSchema,
-  UserSignupSchema200,
-  UserSignupSchema409,
-} from '../schema/auth.js';
+import {UserLogin, UserLoginType, UserSignup, UserSignupType} from '../schema/auth.js';
 import type {FastifyInstance} from 'fastify';
 
 // Create user index for faster query performance
@@ -20,82 +12,52 @@ const collation = {locale: 'en', strength: 2};
 
 const auth = async (fastify: FastifyInstance) => {
   // User login
-  fastify.post<{
-    Body: Static<typeof UserLoginSchema>;
-    Reply: Static<typeof UserLoginSchema200> | Static<typeof UserLoginSchema401>;
-  }>(
-    '/login',
-    {
-      schema: {
-        body: UserLoginSchema,
-        response: {
-          200: UserLoginSchema200,
-          401: UserLoginSchema401,
-        },
-      },
-    },
-    async (req, reply) => {
-      const user = await usersAccounts.findOne({username: req.body.username}, {collation});
+  fastify.post<UserLoginType>('/login', {schema: UserLogin}, async (req, reply) => {
+    const user = await usersAccounts.findOne({username: req.body.username}, {collation});
 
-      // Check if the user exists and password is valid
-      if (user && (await argon2.verify(user.password, req.body.password))) {
-        return {api_key: user.api_key};
-      }
+    // Check if the user exists and password is valid
+    if (user && (await argon2.verify(user.password, req.body.password))) {
+      return {api_key: user.api_key};
+    }
 
-      // Return 401 Unauthorized error if invalid credentials
-      reply.status(401).send('Invalid username or password');
-    },
-  );
+    // Return 401 Unauthorized error if invalid credentials
+    reply.status(401).send('Invalid username or password');
+  });
 
   // User signup
-  fastify.post<{
-    Body: Static<typeof UserSignupSchema>;
-    Reply: Static<typeof UserSignupSchema200> | Static<typeof UserSignupSchema409>;
-  }>(
-    '/signup',
-    {
-      schema: {
-        body: UserSignupSchema,
-        response: {
-          200: UserSignupSchema200,
-          409: UserSignupSchema409,
-        },
-      },
-    },
-    async (req, reply) => {
-      const user = await usersAccounts.findOne({username: req.body.username}, {collation});
+  fastify.post<UserSignupType>('/signup', {schema: UserSignup}, async (req, reply) => {
+    const user = await usersAccounts.findOne({username: req.body.username}, {collation});
 
-      // Check if the user already exists
-      if (user) {
-        reply.status(409).send('An user already exists with that username.');
-        return;
-      }
+    // Check if the user already exists
+    if (user) {
+      reply.status(409).send('An user already exists with that username.');
+      return;
+    }
 
-      // Check if the username is restricted
-      if (RESTRICTED_USERNAMES.includes(req.body.username.toLowerCase())) {
-        reply.status(409).send('Username is not allowed. Please choose a different username.');
-        return;
-      }
+    // Check if the username is restricted
+    if (RESTRICTED_USERNAMES.includes(req.body.username.toLowerCase())) {
+      reply.status(409).send('Username is not allowed. Please choose a different username.');
+      return;
+    }
 
-      // Hash the user password and insert into the database
-      const passwordHash = await argon2.hash(req.body.password);
-      const totalUsers = await usersAccounts.countDocuments(); // Get the total number of users in the database
-      const timeNowIso = dayjs().toISOString();
+    // Hash the user password and insert into the database
+    const passwordHash = await argon2.hash(req.body.password);
+    const totalUsers = await usersAccounts.countDocuments(); // Get the total number of users in the database
+    const timeNowIso = dayjs().toISOString();
 
-      await usersAccounts.insertOne({
-        ...req.body,
-        role: totalUsers === 0 ? 'admin' : 'subscriber', // If this is the first user, set the role to admin, otherwise set it to subscriber
-        api_key: uuidv4(),
-        created_at: timeNowIso,
-        last_seen_at: timeNowIso,
-        current_status: 'active',
-        password: passwordHash, // Store the password hash in the database
-      });
+    await usersAccounts.insertOne({
+      ...req.body,
+      role: totalUsers === 0 ? 'admin' : 'subscriber', // If this is the first user, set the role to admin, otherwise set it to subscriber
+      api_key: uuidv4(),
+      created_at: timeNowIso,
+      last_seen_at: timeNowIso,
+      current_status: 'active',
+      password: passwordHash, // Store the password hash in the database
+    });
 
-      // Return a success message
-      reply.status(200).send('User created successfully.');
-    },
-  );
+    // Return a success message
+    reply.status(200).send('User created successfully.');
+  });
 };
 
 export default auth;
--- src/schema/auth.ts
+++ src/schema/auth.ts
@@ -1,43 +1,60 @@
-import {Type} from '@sinclair/typebox';
+import {Type, Static} from '@sinclair/typebox';
 
 const usernamePattern = '^[0-9a-zA-Z_-]+$';
 
-export const UserLoginSchema = Type.Object({
-  username: Type.String({
-    minLength: 2,
-    maxLength: 16,
+export const UserLogin = {
+  body: Type.Object({
+    username: Type.String({
+      minLength: 2,
+      maxLength: 16,
+    }),
+    password: Type.String({
+      minLength: 6,
+      maxLength: 32,
+    }),
   }),
-  password: Type.String({
-    minLength: 6,
-    maxLength: 32,
+  response: {
+    200: Type.Object({
+      api_key: Type.String(),
+    }),
+    401: Type.Literal('Invalid username or password'),
+  },
+};
+
+export type UserLoginType = {
+  Body: Static<typeof UserLogin.body>;
+  Reply: Static<(typeof UserLogin.response)[200]> | Static<(typeof UserLogin.response)[401]>;
+};
+
+export const UserSignup = {
+  body: Type.Object({
+    displayname: Type.String({
+      minLength: 1,
+      maxLength: 48,
+    }),
+    username: Type.String({
+      minLength: 2,
+      maxLength: 16,
+      pattern: usernamePattern,
+      description:
+        'Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.',
+    }),
+    password: Type.String({minLength: 6, maxLength: 32}),
+    sex: Type.Union([Type.Literal('male'), Type.Literal('female'), Type.Literal('unknown')], {default: 'unknown'}),
   }),
-});
+  response: {
+    200: Type.Literal('User created successfully.'),
+    409: Type.Union([
+      Type.Literal('An user already exists with that username.'),
+      Type.Literal('Username is not allowed. Please choose a different username.'),
+    ]),
+  },
+};
 
-export const UserLoginSchema200 = Type.Object({
-  api_key: Type.String(),
-});
-
-export const UserLoginSchema401 = Type.Literal('Invalid username or password');
-
-export const UserSignupSchema = Type.Object({
-  displayname: Type.String({
-    minLength: 1,
-    maxLength: 48,
-  }),
-  username: Type.String({
-    minLength: 2,
-    maxLength: 16,
-    pattern: usernamePattern,
-    description:
-      'Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.',
-  }),
-  password: Type.String({minLength: 6, maxLength: 32}),
-  sex: Type.Union([Type.Literal('male'), Type.Literal('female'), Type.Literal('unknown')], {default: 'unknown'}),
-});
-
-export const UserSignupSchema200 = Type.Literal('User created successfully.');
-
-export const UserSignupSchema409 = Type.String();
+export type UserSignupType = {
+  Body: Static<typeof UserSignup.body>;
+  Reply: Static<(typeof UserSignup.response)[200]> | Static<(typeof UserSignup.response)[409]>;
+};
 
 export const UserUpdateSchema = Type.Object(
   {

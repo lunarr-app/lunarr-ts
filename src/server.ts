264613f6277d5c7f26b562960b54004df5d4710f
Sayem Chowdhury <sayem314@gmail.com>
2023-04-16 22:38:21 +0600

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import server from './app.js';
 import {env} from './lib/config.js';
+import {logger} from './lib/logger.js';
 
 try {
   // Start server
   await server.listen({port: env.PORT});
 } catch (err: any) {
-  console.error(err.message);
+  logger.fatal(err);
   process.exit(1);
 }

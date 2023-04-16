f9bbefbec8edea6cdd72664a059af52144c72eb5
Sayem Chowdhury <sayem314@gmail.com>
2023-04-16 22:48:29 +0600

    use logger

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,11 +1,15 @@
 import {cleanEnv, str, num, bool} from 'envalid';
 import * as dotenv from 'dotenv';
+import {logger} from './logger.js';
 
-// load env
+logger.info('Loading environment variables');
 dotenv.config();
 
+// Clean and validate environment variables with envalid
 export const env = cleanEnv(process.env, {
   PORT: num({default: 3000}),
   MONGODB_URI: str({default: 'mongodb://127.0.0.1:27017/lunarr'}),
   DISABLE_FASTIFY_LOGGING: bool({default: false}),
 });
+
+logger.info('Environment variables loaded and validated successfully');

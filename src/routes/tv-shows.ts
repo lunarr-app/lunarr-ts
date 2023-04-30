0d6e5272160f5df73fd0690cf2c9473f7ae426b9
Sayem Chowdhury <sayem314@gmail.com>
2023-04-30 14:19:39 +0600

    route tv-show

--- src/routes/tv-shows.ts
+++ src/routes/tv-shows.ts
@@ -1,9 +1,40 @@
+import {tvShowsLists} from '../lib/database.js';
+import {TVShowResultsQuery, TVShowResultsQueryType} from '../schema/tv-show.js';
 import type {FastifyInstance, RouteShorthandOptions} from 'fastify';
+import type {TvShowsList} from '../types/database';
 
 const tvShows = async (fastify: FastifyInstance, options: RouteShorthandOptions) => {
-  // Get tv show lists with pagination and search
-  fastify.get('/', options, async () => {
-    return {to: 'do'};
+  // Get TV show lists with pagination and search
+  fastify.get<TVShowResultsQueryType>('/', {...options, schema: TVShowResultsQuery}, async (req) => {
+    // Extract query parameters using destructuring
+    const {limit, page, query} = req.query;
+
+    // Build query object based on search query
+    const search = query
+      ? {
+          $text: {
+            $search: query,
+            $caseSensitive: false,
+          },
+        }
+      : {};
+
+    // Find TV shows in the database based on query and pagination
+    const totalShows = await tvShowsLists.countDocuments(search);
+    const showList = await tvShowsLists
+      .find(search)
+      .project<Pick<TvShowsList, 'show'>>({show: 1})
+      .skip(page > 0 ? (page - 1) * limit : 0)
+      .limit(limit)
+      .toArray();
+
+    // Return TV shows
+    return {
+      results: showList,
+      limit,
+      page,
+      total: Math.ceil(totalShows / limit),
+    };
   });
 };
 
---
+++ src/schema/tv-show.ts
@@ -0,0 +1,91 @@
+import {Type, Static} from '@sinclair/typebox';
+import {API_HEADERS, SCHEMA_SECURITY} from './auth.js';
+import {TvShowDetails} from '../lib/tmdb/schema/tv-show.js';
+
+// API schema for TV show search results endpoint.
+export const TVShowResultsQuery = {
+  headers: API_HEADERS,
+  querystring: Type.Object({
+    query: Type.Optional(
+      Type.String({
+        description:
+          'The search query string. If provided, search results will be filtered based on this query string.',
+      }),
+    ),
+    limit: Type.Integer({
+      minimum: 1,
+      default: 20,
+      description: 'The number of results to return per page.',
+    }),
+    page: Type.Integer({
+      minimum: 1,
+      default: 1,
+      description: 'The page number of results to return.',
+    }),
+  }),
+  response: {
+    200: Type.Object({
+      results: Type.Array(
+        Type.Object({
+          show: TvShowDetails,
+        }),
+      ),
+      limit: Type.Integer({
+        description: 'The number of results returned per page.',
+      }),
+      page: Type.Integer({
+        description: 'The page number of results returned.',
+      }),
+      total: Type.Integer({
+        description: 'The total number of results that matched the search query.',
+      }),
+    }),
+  },
+  ...SCHEMA_SECURITY,
+};
+
+export type TVShowResultsQueryType = {
+  Headers: Static<typeof TVShowResultsQuery.headers>;
+  Querystring: Static<typeof TVShowResultsQuery.querystring>;
+  Reply: Static<(typeof TVShowResultsQuery.response)[200]>;
+};
+
+// API schema for streaming a TV show episode with specified TMDb ID.
+export const TVShowEpisodeStreamParams = {
+  headers: Type.Composite([
+    API_HEADERS,
+    Type.Object({
+      range: Type.Optional(
+        Type.String({
+          description:
+            'Specifies which part of the file to send based on byte ranges. Only supported in 206 Partial Content responses.',
+        }),
+      ),
+    }),
+  ]),
+  params: Type.Object({
+    tmdb_id: Type.String({
+      description: 'The TMDb ID of the TV show to stream.',
+    }),
+    season: Type.String({
+      description: 'The season number of the TV show episode to stream.',
+    }),
+    episode: Type.String({
+      description: 'The episode number of the TV show episode to stream.',
+    }),
+  }),
+  response: {
+    200: Type.Uint8Array({
+      description: 'The full stream of the TV show if no "range" header is provided.',
+    }),
+    206: Type.Uint8Array({
+      description: 'A partial stream of the TV show if a "range" header is provided.',
+    }),
+  },
+  ...SCHEMA_SECURITY,
+};
+
+export type TVShowEpisodeStreamParamsType = {
+  Headers: Static<typeof TVShowEpisodeStreamParams.headers>;
+  Params: Static<typeof TVShowEpisodeStreamParams.params>;
+};
